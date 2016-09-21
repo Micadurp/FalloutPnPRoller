ba8ef85c495082e5122405dc0d7b2344331cd437
Micadurp <Nicholas.r.DeYoung@gmail.com>
2016-09-21 16:22:47 +0200

    Actually calculates the effect now!

--- RollerApp/src/main/java/me/durp/falloutpnproller/DiceRoller.java
+++ RollerApp/src/main/java/me/durp/falloutpnproller/DiceRoller.java
@@ -29,7 +29,36 @@ public class DiceRoller extends Activity {
 
     private void calculateEffect(){
 
+        TextView skillValue = (TextView) findViewById(R.id.skillValue);
+        TextView plusValue = (TextView) findViewById(R.id.plusValue);
+        TextView minusValue = (TextView) findViewById(R.id.minusValue);
+        TextView diceValue = (TextView) findViewById(R.id.diceValue);
+
+        Integer skill = 0;
+        Integer plus = 0;
+        Integer minus = 0;
+        Integer dice = 0;
+
+        if (!skillValue.getText().toString().equals("")) {
+            skill = Integer.parseInt(skillValue.getText().toString());
+        }
+
+        if (!plusValue.getText().toString().equals("")) {
+            plus = Integer.parseInt(plusValue.getText().toString());
+        }
+
+        if (!minusValue.getText().toString().equals("")) {
+            minus = Integer.parseInt(minusValue.getText().toString());
+        }
+
+        if (!diceValue.getText().toString().equals("")) {
+            dice = Integer.parseInt(diceValue.getText().toString());
+        }
+
         TextView effectValue = (TextView) findViewById(R.id.effectValue);
-        effectValue.setText("IT WORKS");
+
+        Integer effect = skill + plus - minus - dice;
+
+        effectValue.setText(effect.toString());
     }
 }
