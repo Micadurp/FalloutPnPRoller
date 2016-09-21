e85917e5cc3f46d047ddaf63203b562e62087d96
Micadurp <Nicholas.r.DeYoung@gmail.com>
2016-09-21 15:16:50 +0200

    A basic rollmenu with a listening button

--- RollerApp/src/main/java/me/durp/falloutpnproller/DiceRoller.java
+++ RollerApp/src/main/java/me/durp/falloutpnproller/DiceRoller.java
@@ -3,7 +3,10 @@ package me.durp.falloutpnproller;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by enicdey on 9/20/16.
@@ -14,5 +17,19 @@ public class DiceRoller extends Activity {
     protected void onCreate(Bundle pSavedInstanceState) {
         super.onCreate(pSavedInstanceState);
         setContentView(R.layout.activity_dice_roller);
+
+        Button rollButton = (Button) findViewById(R.id.rollButton);
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                calculateEffect();
+            }
+        });
+    }
+
+    private void calculateEffect(){
+
+        TextView effectValue = (TextView) findViewById(R.id.effectValue);
+        effectValue.setText("IT WORKS");
     }
 }
