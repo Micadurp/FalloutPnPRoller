ff52f514f9d89aec3d61656837eeeb2aa86393a8
Micadurp <Nicholas.r.DeYoung@gmail.com>
2016-09-22 12:34:40 +0200

    Added custom font
    Dont know if it actually changed the font, but maybe

--- RollerApp/src/main/java/me/durp/falloutpnproller/FalloutPnPRoller.java
+++ RollerApp/src/main/java/me/durp/falloutpnproller/FalloutPnPRoller.java
@@ -5,11 +5,13 @@ import android.app.Application;
 /**
  * Created by enicdey on 9/20/16.
  */
-public class FalloutPnPRoller extends Application {
+public final class FalloutPnPRoller extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        //Set custom theme for monospace, and thus the whole app
+        FontsOverride.setDefaultFont(this, "MONOSPACE", "monofont.ttf");
     }
 
     @Override
